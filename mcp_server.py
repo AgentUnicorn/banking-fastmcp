627b66b8d4aac5b0fae980cae202bea6e80ff77f
binh.np <binh.np@gianty.com.vn>
2025-11-04 17:55:25 +0700

    fix: update mcp_server.py

--- mcp_server.py
+++ mcp_server.py
@@ -18,6 +18,7 @@ logger = logging.getLogger(__name__)
 mcp = FastMCP(
     name="Banking FastMCP server",
     instructions="You are a banking MCP server which assist users to interact with their account",
+    stateless_http=True,
 )
 mcp.add_middleware(ToolCallMiddleware())
 
